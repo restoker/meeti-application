35ec6a3fa59f62b3f032d89c24f7fbe4b3e8f45c
restoker <milthon.doom@gmail.com>
2026-04-10 11:35:04 -0500

    register form added

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,14 +6,17 @@ export default function AuthLayout({ children }: Readonly<{
 }>) {
     return (
         <>
-            <div className="flex justify-center pt-10">
+            {/* <div className="flex justify-center pt-10">
                 <Link href='/' className="w-48">
                     <Logo />
                 </Link>
+            </div> */}
+            <div className="relative">
+                {/* <video src="https://cdn.cosmos.so/bdf71ccf-67f9-4fe1-b96f-829dc5226636.mp4" autoPlay loop muted className="w-full h-full object-cover absolute top-0 left-0 z-0"></video> */}
+                <main className="">
+                    {children}
+                </main>
             </div>
-            <main className="max-w-2xl mx-auto py-16 px-5">
-                {children}
-            </main>
         </>
     );
 }
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,4 +1,11 @@
+import AuthBackgroundShape from "@/assets/svg/auth-background-shape";
+import RegisterForm from "@/components/shadcn-studio/blocks/register-01/register-form";
+import Logo from "@/components/shadcn-studio/logo";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
     title: "Crear Cuenta"
@@ -6,8 +13,54 @@ export const metadata: Metadata = {
 
 export default function RegisterPage() {
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <>
+            <div className='relative flex h-auto min-h-screen items-center justify-center overflow-x-hidden px-4 py-10 sm:px-6 lg:px-8'>
+                <div className="absolute top-0 left-0 w-full h-full bg-[url('https://cdn.cosmos.so/239de491-9008-45e5-94fa-ae2059ac519c')] bg-cover bg-center hover:mix-blend-difference">
+                    {/* <AuthBackgroundShape /> */}
+
+                </div>
+
+                <Card className='z-1 w-full border-none shadow-md sm:max-w-lg bg-white/20 backdrop-blur-3xl'>
+                    <CardHeader className='gap-6'>
+                        <Image
+                            width={0}
+                            height={0}
+                            sizes="100vw"
+                            src="/img/logo.svg"
+                            alt="Logo"
+                            className="h-12 w-auto"
+                        />
+                        <div>
+                            <CardTitle className='mb-1.5 text-2xl'>Sign Up to Meeti</CardTitle>
+                            <CardDescription className='text-base'>Create your account to get started.</CardDescription>
+                        </div>
+                    </CardHeader>
+
+                    <CardContent>
+                        {/* Register Form */}
+                        <div className='space-y-4'>
+                            <RegisterForm />
+
+                            <p className='text-center text-white'>
+                                Already have an account?{' '}
+                                <a href='#' className='text-white hover:underline'>
+                                    Sign in instead
+                                </a>
+                            </p>
+
+                            <div className='flex items-center gap-4'>
+                                <Separator className='flex-1' />
+                                <p>or</p>
+                                <Separator className='flex-1' />
+                            </div>
+
+                            <Button variant='ghost' className='w-full' asChild>
+                                <a href='#'>Sign in with google</a>
+                            </Button>
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        </>
     );
 }
